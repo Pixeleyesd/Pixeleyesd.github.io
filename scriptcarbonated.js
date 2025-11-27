7df4c4bccc338fcacd5778eea23606b2a22b180f
terrainplatformer <juliushwashere@gmail.com>
2025-11-27 15:16:06 +1300

    10digibutton1

---
+++ scriptcarbonated.js
@@ -0,0 +1,8 @@
+function checkLogin() {
+  if (
+    document.getElementById("user").value === "E8indigi???" &&
+    document.getElementById("pass").value === "E8indigi???"
+  ) {
+    location.href = "welcome.html";
+  }
+}
